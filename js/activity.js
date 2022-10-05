1b091ef9296be05301e33eea2817bb6b7140153d
Shandira Ferguson <shandira.ferguson@gmail.com>
2022-10-04 17:41:30 -0700

    selected display logic complete

--- js/activity.js
+++ js/activity.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
     var selectedColor = 'rgb(158, 208, 52)';
     var unselectedColor = 'rgb(238, 255, 230)';
     var currText;
+    var $displaySelected = $('#displaySelected');
+    var $result = $('#result');
 
     $('table tbody tr td').not('td:first-child').hover(function() {
         currText = $(this).text();
@@ -20,12 +22,33 @@ $(document).ready(function() {
                 fontWeight: 'bold', 
                 cursor: 'pointer'
             })
+
+            //User interaction with table cells
+            $displaySelected.css({
+                visibility:'visible', 
+                marginTop: '2em', 
+            })
+            $result.append('<p>'+currText+'</p>');
+
         } else {
             $(this).css({
                 backgroundColor: unselectedColor, 
                 color: '#000', 
                 fontWeight: 'normal'
-            })    
+            })
+            
+            console.log('FOUND:', $result.find('p').find(currText));
+            // $result.find('p').find(currText).remove(this);
+            // $result.remove($result.find('p').find(currText));
+
+            $('#result p:contains('+currText+')').remove(); //remove child element
+
+            if(!$result.has('p').length) {  //check if there are any child elements within parent
+                $displaySelected.css({
+                    visibility: 'hidden', 
+                    marginTop: '0'
+                })
+            }
         }  
     });
 });
