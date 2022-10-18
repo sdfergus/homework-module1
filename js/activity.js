d1632ee9ebd6adc9c7e266aae9618f9897216d15
Shandira Ferguson <shandira.ferguson@gmail.com>
2022-10-17 22:19:47 -0700

    Bootstrap modal added + js functionality complete

--- js/activity.js
+++ js/activity.js
@@ -6,18 +6,26 @@ $(document).ready(function() {
     var $result = $('#result');
     var $tHead = $('table thead tr th'); 
 
+    //Modal elements
+    var $tableModal = $('#tableModal');
+    var $modalBody = $('.modal-body');
+
+
     $('table tbody tr td').not('td:first-child').hover(function() {
         currText = $(this).text();
         if(currText !== 'Not Available') {
             $(this).css({
                 cursor: 'pointer'
             })
-        }
+        } 
     }).click(function() {
         var currCellColor = $(this).css('backgroundColor');
         var index = $(this).index();
         currText = $(this).text();
-        if(currCellColor !== selectedColor && currText !== 'Not Available') {
+
+        if(currText === 'Not Available') {
+            $tableModal.modal('hide');
+        } else if(currCellColor !== selectedColor && currText !== 'Not Available') {
             $(this).css({
                 backgroundColor: selectedColor, 
                 color: '#fff', 
@@ -26,11 +34,15 @@ $(document).ready(function() {
             })
 
             //User interaction with table cells
-            $displaySelected.css({
-                visibility:'visible', 
-                marginTop: '2em', 
-            })
-            $result.append('<p>'+currText+ ' at ' + $tHead.eq(index).text() + '</p>');
+            // $displaySelected.css({
+            //     visibility:'visible', 
+            //     marginTop: '2em', 
+            // })
+            // $result.append('<p>'+currText+ ' at ' + $tHead.eq(index).text() + '</p>');
+
+            //Show Modal and contents
+            $tableModal.modal('show');
+            $modalBody.append('<p>'+currText+ ' at ' + $tHead.eq(index).text() + '</p>');
 
         } else {
             $(this).css({
@@ -43,7 +55,11 @@ $(document).ready(function() {
             // $result.find('p').find(currText).remove(this);
             // $result.remove($result.find('p').find(currText));
 
-            $('#result p:contains('+currText+')').remove(); //remove child element
+            // $('#result p:contains('+currText+')').remove(); //remove child element
+            
+            //Remove modal child element + show result
+            $('.modal-body p:contains('+currText+')').remove(); 
+            $tableModal.modal('show');
 
             if(!$result.has('p').length) {  //check if there are any child elements within parent
                 $displaySelected.css({
